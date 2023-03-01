6e790dd96e8c17259869a108f2f9238062eff5af
applick-hash <0bpq@proton.me>
2023-03-01 15:03:23 -0800

    rustint/test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+// import 'dart:async';
 
 // import 'package:flutter/services.dart';
-// import 'package:xlr/xlr.dart';
+import 'package:xlr/xlr.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,12 +16,59 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late Future<int> counter;
+  // String _platformVersion = 'Unknown';
+  final _xlrPlugin = Xlr();
+
+  @override
+  void initState() {
+    super.initState();
+    // initPlatformState();
+    counter = Xlr().getcounterfromrust();
+  }
+
+  // // Platform messages are asynchronous, so we initialize in an async method.
+  // Future<void> initPlatformState() async {
+  //   String platformVersion;
+  //   // Platform messages may fail, so we use a try/catch PlatformException.
+  //   // We also handle the message potentially returning null.
+  //   try {
+  //     platformVersion =
+  //         await _xlrPlugin.getPlatformVersion() ?? 'Unknown platform version';
+  //   } on PlatformException {
+  //     platformVersion = 'Failed to get platform version.';
+  //   }
+
+  //   // If the widget was removed from the tree while the asynchronous platform
+  //   // message was in flight, we want to discard the reply rather than calling
+  //   // setState to update our non-existent appearance.
+  //   if (!mounted) return;
+
+  //   setState(() {
+  //     _platformVersion = platformVersion;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('xlr app'),
+        ),
+        body: Center(
+          // child: Text('Running on: $_platformVersion\n'),
+          // child: Text("plugin text"),
+          child: FutureBuilder(
+            future: Future.wait([counter]),
+            builder: (context, snap) {
+              final data = snap.data;
+              if (data == null) {
+                return const Text("loading...");
+              }
+              return Text('${data[0]}');
+            },
+          ),
         ),
       ),
     );
--- lib/xlr.dart
+++ lib/xlr.dart
@@ -1,7 +1,28 @@
-
+// import 'xlr_platform_interface.dart';
 
 // class Xlr {
-//   Future<Int32?> {
-//     return 0;
+//   Future<String?> getPlatformVersion() {
+//     return XlrPlatform.instance.getPlatformVersion();
 //   }
 // }
+
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:xlr/bridge_generated.dart';
+
+const base = "interface";
+final path = Platform.isWindows ? "$base.dll" : "lib$base.so";
+final dylib = Platform.isIOS
+    ? DynamicLibrary.process()
+    : Platform.isMacOS
+        ? DynamicLibrary.executable()
+        : DynamicLibrary.open(path);
+
+final api = InterfaceImpl(dylib);
+
+class Xlr {
+  Future<int> getcounterfromrust() {
+    return api.getCount();
+  }
+}
